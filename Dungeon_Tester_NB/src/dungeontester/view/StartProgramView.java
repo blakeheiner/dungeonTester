54978a6bd6ec47db5af987c7f2414030e971b900
blakeheiner <heiner.blake@gmail.com>
2015-10-28 12:54:15 -0600

    StartProgramView added as well as the view package layer

---
+++ Dungeon_Tester_NB/src/dungeontester/view/StartProgramView.java
@@ -0,0 +1,23 @@
+/*
+  startProgram(): void
+ BEGIN
+  Display the banner screen
+  Get the players name
+  Create a new player
+  DISPLAY a customized welcome message
+  DISPLAY the main menu
+ END
+ */
+package dungeontester.view;
+
+/**
+ *
+ * @author blakeheiner
+ */
+public class StartProgramView {
+    
+    public static void startProgram(){
+        
+    }
+    
+}
