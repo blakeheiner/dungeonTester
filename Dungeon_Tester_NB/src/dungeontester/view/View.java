57c55b9584541ecd8d58fd43f9f42d5f71add8d4
blakeheiner <heiner.blake@gmail.com>
2015-11-12 15:13:02 -0700

    team assignment 8 - added View.java and ViewInterface.java (both are unfinished though)

---
+++ Dungeon_Tester_NB/src/dungeontester/view/View.java
@@ -0,0 +1,50 @@
+/*
+ * 
+ */
+package dungeontester.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author blakeheiner
+ */
+public abstract class View implements ViewInterface {
+   
+    private String promptMessage;
+    
+    @Override
+    public void display(){
+        String value = "";
+        boolean done = false;
+        
+        do{
+            System.out.println(this.promptMessage); //display prompt message
+            value = this.getInput(); // get value end user entered
+            done = this.doAction(value); // do action based on value entered
+        }while (!done);
+    }
+    
+    @Override
+    public String getInput(){
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        String value = null;
+        
+        //while a valid name has not been retrieved
+        while (!valid){
+            
+            //get the value entered from the keyboard
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if(value.length() < 1){
+                System.out.println("You must enter a value.");
+                continue;
+            }
+            break;
+        }
+        
+        return value;
+    }
+}
---
+++ Dungeon_Tester_NB/src/dungeontester/view/ViewInterface.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+package dungeontester.view;
+
+/**
+ *
+ * @author blakeheiner
+ */
+public interface ViewInterface {
+    
+    public void display();
+    public String getInput();
+    public boolean doAction(Object obj);
+    
+}
